31c530028b346e15ced3a4abd1d29b3631bbee72
Lucas Ball <balll@flip3.engr.oregonstate.edu>
2021-04-19 19:49:14 -0700

    updated

--- tests.py
+++ tests.py
@@ -2,100 +2,13 @@ import random
 import unittest
 from credit_card_validator import credit_card_validator
 
-if __name__ == '__main__':
-    unittest.main()
-
 class TestCase(unittest.TestCase):
     def test1(self):
-        val = 101
-        self.assertTrue(credit_card_validator(val))
+        val = '101'
+        self.assertFalse(credit_card_validator(val), msg='{} does not meet requirements'.format(val))
+    def test2(self):
+        val = '4535893340572712'
+        self.assertTrue(credit_card_validator(val), msg='{} does not meet requirements'.format(val))
 
-'''
-def random():
-    for x in range(10):
-        num = '4'
-        for i in range(15):
-            digit = str(random.randint(0, 9))
-            num += digit
-        if(not credit_card_validator(num)):
-            print("found a bug?")
-            return False
-    for x in range(10):
-        num = '5'
-        num += str(random,randint(1, 5))
-        for i in range(14):
-            digit = str(random.randint(0, 9))
-            num += digit
-        if(not credit_card_validator(num)):
-            print("found a bug?")
-            return False
-    for x in range(10):
-        num = '2'
-        num += str(random,randint(221, 720))
-        for i in range(12):
-            digit = str(random.randint(0, 9))
-            num += digit
-        if(not credit_card_validator(num)):
-            print("found a bug?")
-            return False
-    for x in range(10):
-        num = '3'
-        num += str(random,randint(4, 7))
-        for i in range(13):
-            digit = str(random.randint(0, 9))
-            num += digit
-        if(not credit_card_validator(num)):
-            print("found a bug?")
-            return False
-
-def luhn(num):
-    total = 0
-    for i in range(len(num)):
-        if (i % 2 == 1):
-            total += int(num[i])
-    return ((total * 9) % 10)
-
-def randomLuhn():
-    for x in range(10):
-        num = '4'
-        for i in range(14):
-            digit = str(random.randint(0, 9))
-            num += digit
-        num += str(luhn(num))
-        if(not credit_card_validator(num)):
-            print("found a bug?")
-            return False
-    for x in range(10):
-        num = '5'
-        num += str(random,randint(1, 5))
-        for i in range(13):
-            digit = str(random.randint(0, 9))
-            num += digit
-        num += str(luhn(num))
-        if(not credit_card_validator(num)):
-            print("found a bug?")
-            return False
-    for x in range(10):
-        num = '2'
-        num += str(random,randint(221, 720))
-        for i in range(11):
-            digit = str(random.randint(0, 9))
-            num += digit
-        num += str(luhn(num))
-        if(not credit_card_validator(num)):
-            print("found a bug?")
-            return False
-    for x in range(10):
-        num = '3'
-        num += str(random,randint(4, 7))
-        for i in range(12):
-            digit = str(random.randint(0, 9))
-            num += digit
-        num += str(luhn(num))
-        if(not credit_card_validator(num)):
-            print("found a bug?")
-            return False
-
-random()
-randomLuhn()
-'''
+if __name__ == '__main__':
+    unittest.main()
