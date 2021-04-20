416a9c300e3b7a710c6020d5ed5753278ed64e27
Lucas Ball <balll@flip3.engr.oregonstate.edu>
2021-04-19 19:53:57 -0700

    updated

--- tests.py
+++ tests.py
@@ -12,8 +12,17 @@ class TestCase(unittest.TestCase):
     def test3(self):
         val = '5372076718012718'
         self.assertTrue(credit_card_validator(val), msg='{} does not meet requirements'.format(val))
-    def test3(self):
+    def test4(self):
         val = '349852453816112'
+        self.assertTrue(credit_card_validator(val), msg='{} does not meet requirements'.format(val))
+    def test5(self):
+        val = '3498524538161122'
+        self.assertFalse(credit_card_validator(val), msg='{} does not meet requirements'.format(val))
+    def test6(self):
+        val = '5372076718012717'
+        self.assertFalse(credit_card_validator(val), msg='{} does not meet requirements'.format(val))
+    def test7(self):
+        val = '4535893340572713'
         self.assertTrue(credit_card_validator(val), msg='{} does not meet requirements'.format(val))
     
 
