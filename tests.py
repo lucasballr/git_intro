0ce9c3ad9f3553a359a3f300a25cc32265b0e0e5
Lucas Ball <balll@flip3.engr.oregonstate.edu>
2021-04-26 13:52:54 -0700

    updated

--- tests.py
+++ tests.py
@@ -7,24 +7,24 @@ class TestCase(unittest.TestCase):
 
     def test1(self):
         val = 155
-        self.assertFalse(contrived_func(val), msg='incorrect')
+        self.assertTrue(contrived_func(val), msg='incorrect')
     def test2(self):
-        val = 155
+        val = 156
         self.assertFalse(contrived_func(val), msg='incorrect')
     def test3(self):
-        val = 155
+        val = 6
         self.assertFalse(contrived_func(val), msg='incorrect')
     def test4(self):
-        val = 155
-        self.assertFalse(contrived_func(val), msg='incorrect')
+        val = 17
+        self.assertTrue(contrived_func(val), msg='incorrect')
     def test5(self):
-        val = 155
-        self.assertFalse(contrived_func(val), msg='incorrect')
+        val = 120
+        self.assertTrue(contrived_func(val), msg='incorrect')
     def test6(self):
-        val = 155
-        self.assertFalse(contrived_func(val), msg='incorrect')
+        val = 55
+        self.assertTrue(contrived_func(val), msg='incorrect')
     def test7(self):
-        val = 155
+        val = 56
         self.assertFalse(contrived_func(val), msg='incorrect')
 
 
