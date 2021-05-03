3c18c5099146c7817b965afbf63cbf5dfa3bafbd
Lucas Ball <balll@flip3.engr.oregonstate.edu>
2021-05-03 15:31:02 -0700

    updated

--- tests.py
+++ tests.py
@@ -7,53 +7,53 @@ class TestCase(unittest.TestCase):
 
     # Valid Visa
     def test1(self):
-        for i in range(1000):
+        for i in range(10000):
             visa = visaGen()
             credit_card_validator(visa)
 
     # Valid Mastercard
     def test2(self):
-        for i in range(1000):
+        for i in range(10000):
             master = masterGen()
             credit_card_validator(master)
 
     # Valid MasterCard 2
     def test3(self):
-        for i in range(1000):
+        for i in range(10000):
             masterTwo = masterGen2()
             credit_card_validator(masterTwo)
 
     # Valid AMEX
     def test4(self):
-        for i in range(1000):
+        for i in range(10000):
             amex = amexGen()
             credit_card_validator(amex)
 
     # 16 Digit Invalid Prefix
     def test5(self):
-        for i in range(1000):
+        for i in range(10000):
             invalid = invalidPref('1')
             credit_card_validator(invalid)
-        for i in range(1000):
+        for i in range(10000):
             invalid = invalidPref('8')
             credit_card_validator(invalid)
 
     # 15 digit Invalid prefix
     def test6(self):
-        for i in range(1000):
+        for i in range(10000):
             invalShort = invalidPrefShort('1')
             credit_card_validator(invalShort)
-        for i in range(1000):
+        for i in range(10000):
             invalShort = invalidPrefShort('8')
             credit_card_validator(invalShort)
 
     # Invalid luhn
     def test7(self):
-        for i in range(1000):
+        for i in range(10000):
             invalShort = invalidPrefShort('4')
             invalShort += str(random.ranint(0, 9))
             credit_card_validator(invalShort)
-        for i in range(1000):
+        for i in range(10000):
             invalShort = invalidPrefShort('3')
             invalShort += str(random.ranint(0, 9))
             credit_card_validator(invalShort)
