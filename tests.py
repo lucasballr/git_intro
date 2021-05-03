900981d60c8e780b4713133bde2adff2840b1e67
Lucas Ball <balll@flip3.engr.oregonstate.edu>
2021-05-03 15:23:07 -0700

    updated

--- tests.py
+++ tests.py
@@ -10,7 +10,7 @@ class TestCase(unittest.TestCase):
         for i in range(1000):
             visa = visaGen()
             credit_card_validator(visa)
-    
+
     # Valid Mastercard
     def test2(self):
         for i in range(1000):
@@ -29,7 +29,7 @@ class TestCase(unittest.TestCase):
             amex = amexGen()
             credit_card_validator(amex)
 
-    # Invalid Pref
+    # 16 Digit Invalid Prefix
     def test5(self):
         for i in range(1000):
             invalid = invalidPref('1')
@@ -38,6 +38,7 @@ class TestCase(unittest.TestCase):
             invalid = invalidPref('8')
             credit_card_validator(invalid)
 
+    # 15 digit Invalid prefix
     def test6(self):
         for i in range(1000):
             invalShort = invalidPrefShort('1')
@@ -45,61 +46,78 @@ class TestCase(unittest.TestCase):
         for i in range(1000):
             invalShort = invalidPrefShort('8')
             credit_card_validator(invalShort)
-    
+
+    # Invalid luhn
     def test7(self):
         for i in range(1000):
             invalShort = invalidPrefShort('4')
-            invalShort += str(random.ranint(0,9))
+            invalShort += str(random.ranint(0, 9))
             credit_card_validator(invalShort)
         for i in range(1000):
             invalShort = invalidPrefShort('3')
-            invalShort += str(random.ranint(0,9))
+            invalShort += str(random.ranint(0, 9))
             credit_card_validator(invalShort)
 
+    # Random numbers
     def test8(self):
         for i in range(100000):
             randomNum = completelyRandom()
             credit_card_validator(randomNum)
 
+
+# Generates completely random numbers with in a range of sizes
 def completelyRandom():
     val = str(random.randint(1000000000000, 10000000000000000))
-    return val    
+    return val
 
+
+# Generates 16 digit with wrong prefix
 def invalidPref(inp):
     val = inp
     val += str(random.randint(10000000000000, 99999999999999))
     val += str(luhn(val))
     return val
 
+
+# Generates 15 digit with wrong prefix
 def invalidPrefShort(inp):
     val = inp
     val += str(random.randint(1000000000000, 9999999999999))
     val += str(luhn(val))
     return val
 
+
+# Generates Valid Visa
 def visaGen():
     val = '4'
     val += str(random.randint(10000000000000, 99999999999999))
     val += str(luhn(val))
     return val
 
+
+# Generates Valid MasterCard
 def masterGen():
     val = '5'
     val += str(random.randint(10000000000000, 59999999999999))
     val += str(luhn(val))
     return val
 
+
+# Generates Valid MasterCard
 def masterGen2():
     val = '2'
     val += str(random.randint(22100000000000, 72099999999999))
     val += str(luhn(val))
     return val
 
+
+# Generates Valid Amex
 def amexGen():
     val = '3'
     val += str(random.randint(4000000000000, 7999999999999))
     val += str(luhn(val))
     return val
+
 
 # Luhn Calculation
 def luhn(val):
@@ -107,7 +125,7 @@ def luhn(val):
     sum = 0
     for i in range(digits):
         add = 0
-        if (i % 2 == 0):    
+        if (i % 2 == 0):
             add = int(val[digits - i - 1]) * 2
         else:
             add = int(val[digits - i - 1])
